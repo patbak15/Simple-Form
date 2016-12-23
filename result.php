8f330f8b632b32adaa1bbcfa77405490df1847ff
Patrick Aung <patbak15@mail.sfsu.edu>
2016-12-23 15:07:34 -0800

    added all fields in their own row

--- result.php
+++ result.php
@@ -49,16 +49,98 @@
 			</div>
 		</div>
 		<!-- END NAME --> 
+		<div class="row">
+			<label for="gender" class="col-sm-3 control-label">Gender</label> 
+			<div class ="col-sm-9">
+				<?php echo $_POST["gender"]; ?>
+			</div>
+		</div> 
 
+		<div class="row">
+			<label for="eduction" class="col-sm-3 control-label">Education*</label>
+			<div class ="col-sm-9">
+				<?php echo $_POST["eduction"]; ?>
+			</div>
+		</div>
 
+		<div class="row">
+			<label for="phone" class="col-sm-3 control-label">Phone*</label>
+			<div class ="col-sm-9">
+				<?php echo $_POST["phone"]; ?>
+			</div>
+		</div>
 
+		<div class="row">
+			<label for="email" class="col-sm-3 control-label">Email*</label>
+			<div class ="col-sm-9">
+				<?php echo $_POST["email"]; ?>
+			</div>
+		</div>
 
 	</fieldset>
 </div>
 
+<div class="container">
+	<fieldset>
+		<legend>Incident Report</legend>
+		<div class="row">
+			<label for="date" class="col-sm-3 control-label">Date*</label>
+			<div class="col-sm-9">
+				<?php echo $_POST["date"]; ?> 
+			</div>
+		</div>
+		<h4>Location of Incident</h4>
+		<div class="row">
+			<label for="address" class="col-sm-3 control-label">Address*</label>
+			<div class="col-sm-9">
+				<?php echo $_POST["address"]; ?> 
+			</div>
+		</div>
+		<div class="row">
+			<label for="city" class="col-sm-3 control-label">City/Town*</label>
+			<div class="col-sm-9">
+				<?php echo $_POST["city"]; ?> 
+			</div>
+		</div>
 
+		<div class="row">
+			<label for="state" class="col-sm-3 control-label">State*</label>
+			<div class="col-sm-9">
+				<?php echo $_POST["state"]; ?> 
+			</div>
+		</div>
 
+		<div class="row">
+			<label for="zip" class="col-sm-3 control-label">Zip*</label>
+			<div class="col-sm-9">
+				<?php echo $_POST["zip"]; ?> 
+			</div>
+		</div>
 
+		<div class="row">
+			<label for="address" class="col-sm-3 control-label">Catagory*</label>
+			<div class="col-sm-9">
+				<?php echo $_POST["catagory"]; ?> 
+			</div>
+		</div>
+
+		<div class="row">
+			<label for="description" class="col-sm-3 control-label">Description*</label>
+			<div class="col-sm-9">
+				<?php echo $_POST["description"]; ?> 
+			</div>
+		</div>
+
+		<div class="row">
+			<label for="image" class="col-sm-3 control-label">Image*</label>
+			<div class="col-sm-9">
+				<?php $image = $_POST["address"]; ?> 
+				<img src="restul.php?<?php echo $image?>"/>
+			</div>
+		</div>
+
+	</fieldset>
+</div>
 
 
 </html> 
