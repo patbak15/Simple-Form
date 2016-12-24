be2b0e8ca0328fa0d1636ae299ac65f465a1a3f2
Patrick Aung <patbak15@mail.sfsu.edu>
2016-12-24 02:46:45 -0800

    changes to php image, testing results

--- result.php
+++ result.php
@@ -4,7 +4,49 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="validator.js"></script>
-
+  <?php
+  	//script to handle uploading file 
+	$target_dir = "uploads/";
+	$target_file = $target_dir . basename($_FILES["image"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+	    $check = getimagesize($_FILES["image"]["tmp_name"]);
+	    if($check !== false) {
+	        //echo "File is an image - " . $check["mime"] . ".";
+	        $uploadOk = 1;
+	    } else {
+	        //echo "File is not an image.";
+	        $uploadOk = 0;
+	    }
+	}
+	// Check if file already exists
+	if (file_exists($target_file)) {
+	    //echo "Sorry, file already exists.";
+	    $uploadOk = 0;
+	}
+	// Check file size
+	if ($_FILES["image"]["size"] > 500000) {
+	    //echo "Sorry, your file is too large.";
+	    $uploadOk = 0;
+	}
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	&& $imageFileType != "gif" ) {
+	    //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	    $uploadOk = 0;
+	}
+	if ($uploadOk == 0) {
+	    //echo "Sorry, your file was not uploaded.";
+	} else {
+	    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+	        //echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
+	    } else {
+	        //echo "Sorry, there was an error uploading your file.";
+	    }
+	}
+?>
 <head>
 	<title>SFSU CS Department - CSC 642 Fall 2016 Project: Form UI
 	Patrick Aung, patbak15@mail.sfsu.edu </title>
@@ -138,14 +180,9 @@
 		<div class="row">
 			<label for="image" class="col-sm-3 control-label">Image*</label>
 			<div class="col-sm-9">
-				<img scr="
-				<?php
-					if (isset($_FILES['image'])) {
-    					$aExtraInfo = getimagesize($_FILES['image']['tmp_name']);
-    					$sImage = "data:" . $aExtraInfo["mime"] . ";base64," . base64_encode(file_get_contents($_FILES['image']['tmp_name']));
-    					echo $sImage; 
-    				}
-    			?>"/>
+				<?php 
+					echo '<img src="'.$target_file."'/>"; 
+				?>
 			</div>
 		</div>
 
